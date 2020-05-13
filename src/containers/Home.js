53263ab54127c19db42b8c393732b8fec4e67ee4
Daniel Williams <danswilliams95@gmail.com>
2020-05-12 21:56:02 -0700

    Customized input fields.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -15,26 +15,27 @@ const Home = (props) => {
                 <h3> Welcome, {props.userName.split(' ')[0]}! </h3>
                 <div class='container mt-8'>
                     <div class="input-group mb-3">
-                        <select class="custom-select" id="inputGroupSelect02">
+                        <select class="custom-select" id="filter">
                             <option selected>Villagers...</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                            <option value="celeste">Celeste</option>
+                            <option value="daisy">Daisy</option>
+                            <option value="dont-care">Don't Care</option>
                         </select>
-                        <select class="custom-select" id="inputGroupSelect02">
+                        <select class="custom-select" id="filter">
                             <option selected>Categories...</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                            <option value="turnips">Turnips</option>
+                            <option value="cataloging">Cataloging</option>
+                            <option value="crafting">Crafting</option>
+                            <option value="other">Other</option>
                         </select>
-                        <select class="custom-select" id="inputGroupSelect02">
+                        <select class="custom-select" id="filter">
                             <option selected>Fees...</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                            <option value="dont-care">Don't Care</option>
                         </select>
                         <div class="input-group-append">
-                            <label class="input-group-text" for="inputGroupSelect02">Find</label>
+                            <label class="input-group-text" for="filter">Find</label>
                         </div>
                     </div>
                     <div class='card-deck text-center'>
@@ -46,7 +47,7 @@ const Home = (props) => {
                                 <p class='card-text'>View your islands here, or have them sent to you.</p>
                             </div>
                         </div>
-                     
+
                     </div>
                 </div>
             </div>
