6b0f5ba30efc84db18b97233d3bb684e89a0013b
Daniel Williams <danswilliams95@gmail.com>
2020-05-13 21:59:00 -0700

    Updated README. Added a keyword list, transitions to come next.

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ## Installation
 
 - `git clone https://github.com/Better-Turnip-Exchange/front-end`
+- Run `npm install` to install all dependencies.
 - Run `npm start` inside the repo's directory on your machine.
 - Go to `localhost:3000`
--- src/containers/Select.js
+++ src/containers/Select.js
@@ -1,44 +1,73 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+
+import './Select.css';
 
 
 const Select = props => {
     const [villagers, setVillagers] = useState('');
     const [categories, setCategories] = useState('');
     const [fees, setFees] = useState('');
+    const [keyword, setKeyword] = useState('');
+    const [keywords, setKeywords] = useState(['tip', 'gold', 'miles'])
+    const [price, setPrice] = useState('500');
     const [userFilters, setUserFilters] = useState({});
     const bundleFilters = () => {
-        
+
+    }
+    const handlePrice = (e) => {
+        setPrice(e.target.value);
+    }
+    const addKeyword = (e) => {
+        e.preventDefault()
+        console.log(keyword)
+        let newWord = keyword.trim()
+        if (newWord.length > 0 && !keywords.includes(newWord)) {
+            setKeywords([...keywords, newWord])
+        }
+        setKeyword('')
+    }
+    const removeKeyword = (e) => {
+        e.preventDefault()
+        console.log(e)
+        console.log(e.target)
+        let removed = e.currentTarget.id;
+        console.log(removed)
+        setKeywords(keywords.filter(word => word !== removed))
+        console.log(keywords)
+    }
+    const handleKeyword = (e) => {
+        setKeyword(e.target.value);
     }
 
     return (
-        <div class='container mt-4'>
-            <h3> Welcome, {props.userName.split(' ')[0]}! </h3>
-            <div class='container mt-8'>
-
-                <form class="input-group mb-3" onSubmit={bundleFilters}>
-                    <select class="custom-select" id="filter">
-                        <option selected>Villagers...</option>
-                        <option value="celeste">Celeste</option>
-                        <option value="daisy">Daisy</option>
-                        <option value="dont-care">Don't Care</option>
-                    </select>
-                    <select class="custom-select" id="filter">
-                        <option selected>Categories...</option>
-                        <option value="turnips">Turnips</option>
-                        <option value="cataloging">Cataloging</option>
-                        <option value="crafting">Crafting</option>
-                        <option value="other">Other</option>
-                    </select>
-                    <select class="custom-select" id="filter">
-                        <option selected>Fees...</option>
-                        <option value="yes">Yes</option>
-                        <option value="no">No</option>
-                        <option value="dont-care">Don't Care</option>
-                    </select>
-                    <div class="input-group-append">
-                        <button class="input-group-text" for="filter">Find</button>
+        <div class='container mt-4 items-center'>
+            <h3 class='welcome-message'> Welcome, {props.userName.split(' ')[0]}! </h3>
+            <div class='mt-2'>
+                <div class='keywords-wrapper flex mb-4'>
+                    <div class='keywords-header'>
+                        <h1>Keywords</h1>
+                        <h5>We'll go ahead and ignore these keywords while finding islands for you. Feel free to remove any!</h5>
                     </div>
-                </form>
+
+                    <ul class='keyword-list'>
+                        {keywords && keywords.map((keyword, index) =>
+                            <div type='button' class='keyword-label tag label btn-info mr-2' >
+                                <span>{keyword}</span>
+                                <a id={keyword} onClick={removeKeyword}><FontAwesomeIcon icon={faTimesCircle} size='xs' /></a>
+                            </div>
+                        )}
+                    </ul>
+
+                </div>
+
+
+                <div class="input-group price-input mb-2">
+                    <h1>How Many Bells?</h1>
+                    <input type="text" name='price-input' class=" price-input" value={price} min='0' max="999" onChange={handlePrice}></input>
+                </div>
+
                 <div class='card-deck text-center'>
                     <div class='card box-shadow'>
                         <h3 class='card-header'>
@@ -51,7 +80,7 @@ const Select = props => {
 
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 export default Select;
