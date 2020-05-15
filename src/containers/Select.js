41532c7d10231bad316f1edf9287b252a0edf203
kenny <keacolli@ucsc.edu>
2020-05-14 18:43:02 -0700

    Implemented toggable keywords

--- src/containers/Select.js
+++ src/containers/Select.js
@@ -1,86 +1,99 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 import './Select.css';
+import keys from '../config';
 
+const Select = ({ userName }) => {
+  const initialKeywords = {
+    tip: true,
+    gold: true,
+    miles: true,
+    entry: false,
+    nmts: false,
+  };
+  const [villagers, setVillagers] = useState('');
+  const [categories, setCategories] = useState('');
+  const [fees, setFees] = useState('');
+  const [keyword, setKeyword] = useState('');
+  //const [keywords, setKeywords] = useState(['tip', 'gold', 'miles']);
+  const [keywords, setKeywords] = useState(initialKeywords);
+  const [price, setPrice] = useState('500');
+  const [userFilters, setUserFilters] = useState({});
+  const bundleFilters = () => {};
+  const handlePrice = e => {
+    setPrice(e.target.value);
+  };
 
-const Select = props => {
-    const [villagers, setVillagers] = useState('');
-    const [categories, setCategories] = useState('');
-    const [fees, setFees] = useState('');
-    const [keyword, setKeyword] = useState('');
-    const [keywords, setKeywords] = useState(['tip', 'gold', 'miles'])
-    const [price, setPrice] = useState('500');
-    const [userFilters, setUserFilters] = useState({});
-    const bundleFilters = () => {
+  const toggleKeyword = e => {
+    e.preventDefault();
+    console.log(e.currentTarget.id);
+    let newKeywords = { ...keywords };
+    newKeywords[e.currentTarget.id] = !newKeywords[e.currentTarget.id];
+    setKeywords(newKeywords);
+    //keywords[e.currentTarget.id] = !keywords[e.currentTarget.id];
+    console.log(keywords);
+  };
+  const handleKeyword = e => {
+    setKeyword(e.target.value);
+  };
 
-    }
-    const handlePrice = (e) => {
-        setPrice(e.target.value);
-    }
-    const addKeyword = (e) => {
-        e.preventDefault()
-        console.log(keyword)
-        let newWord = keyword.trim()
-        if (newWord.length > 0 && !keywords.includes(newWord)) {
-            setKeywords([...keywords, newWord])
-        }
-        setKeyword('')
-    }
-    const removeKeyword = (e) => {
-        e.preventDefault()
-        console.log(e)
-        console.log(e.target)
-        let removed = e.currentTarget.id;
-        console.log(removed)
-        setKeywords(keywords.filter(word => word !== removed))
-        console.log(keywords)
-    }
-    const handleKeyword = (e) => {
-        setKeyword(e.target.value);
-    }
+  return (
+    <div class='container mt-4 items-center'>
+      <h3 class='welcome-message'> Welcome, {userName.split(' ')[0]}! </h3>
+      <div class='mt-2'>
+        <div class='keywords-wrapper flex mb-4'>
+          <div class='keywords-header'>
+            <h1>Keywords</h1>
+            <h5>
+              We'll go ahead and ignore these keywords while finding islands for
+              you. Feel free to remove any!
+            </h5>
+          </div>
 
-    return (
-        <div class='container mt-4 items-center'>
-            <h3 class='welcome-message'> Welcome, {props.userName.split(' ')[0]}! </h3>
-            <div class='mt-2'>
-                <div class='keywords-wrapper flex mb-4'>
-                    <div class='keywords-header'>
-                        <h1>Keywords</h1>
-                        <h5>We'll go ahead and ignore these keywords while finding islands for you. Feel free to remove any!</h5>
-                    </div>
+          <ul class='keyword-list'>
+            {Object.keys(keywords).map(keyword => (
+              <div
+                type='button'
+                class={`keyword-label tag label btn-info mr-2 ${
+                  keywords[keyword] ? 'btn-primary' : 'btn-light'
+                }`}
+              >
+                <span>{keyword}</span>
+                <a id={keyword} onClick={toggleKeyword}>
+                  <FontAwesomeIcon icon={faTimesCircle} size='xs' />
+                </a>
+              </div>
+            ))}
+          </ul>
+        </div>
 
-                    <ul class='keyword-list'>
-                        {keywords && keywords.map((keyword, index) =>
-                            <div type='button' class='keyword-label tag label btn-info mr-2' >
-                                <span>{keyword}</span>
-                                <a id={keyword} onClick={removeKeyword}><FontAwesomeIcon icon={faTimesCircle} size='xs' /></a>
-                            </div>
-                        )}
-                    </ul>
+        <div class='input-group price-input mb-2'>
+          <h1>How Many Bells?</h1>
+          <input
+            type='text'
+            name='price-input'
+            class=' price-input'
+            value={price}
+            min='0'
+            max='999'
+            onChange={handlePrice}
+          ></input>
+        </div>
 
-                </div>
-
-
-                <div class="input-group price-input mb-2">
-                    <h1>How Many Bells?</h1>
-                    <input type="text" name='price-input' class=" price-input" value={price} min='0' max="999" onChange={handlePrice}></input>
-                </div>
-
-                <div class='card-deck text-center'>
-                    <div class='card box-shadow'>
-                        <h3 class='card-header'>
-                            Islands
-                                </h3>
-                        <div class='card-body'>
-                            <p class='card-text'>View your islands here, or have them sent to you.</p>
-                        </div>
-                    </div>
-
-                </div>
+        <div class='card-deck text-center'>
+          <div class='card box-shadow'>
+            <h3 class='card-header'>Islands</h3>
+            <div class='card-body'>
+              <p class='card-text'>
+                View your islands here, or have them sent to you.
+              </p>
             </div>
-        </div >
-    )
-}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Select;
