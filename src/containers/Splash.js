5ef2d7fe1306e80127438f48b0e9a9441a0da44d
kenny <keacolli@ucsc.edu>
2020-05-19 22:05:37 -0700

    Add card styles class

--- src/containers/Splash.js
+++ src/containers/Splash.js
@@ -3,10 +3,7 @@ import React from 'react';
 const Splash = props => {
   return (
     <div className='flex justify-center items-center mt-10 md:mt-40'>
-      <div
-        id='welcome-wrapper'
-        class='text-center bg-acLight rounded-lg shadow-xl p-10 mx-10'
-      >
+      <div id='welcome-wrapper' class='card text-center p-10 mx-10'>
         <h1 class='title'>The</h1>
         <h1 class='title'>Better Turnip Exchange</h1>
         <a class='btn btn-blue mt-4' href='/find'>
