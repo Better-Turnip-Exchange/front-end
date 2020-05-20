3ee2a29edb0f11ebfa0b9e7fb691e172b1de4af0
kenny <keacolli@ucsc.edu>
2020-05-19 21:31:02 -0700

    fix background h-screen thing, and made background green

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './build.css';
 
 function App() {
   return (
-    <div className='bg-acBlue h-screen'>
+    <div className='h-full'>
       <div
         id='navbar'
-        className='flex flex-wrap p-3 items-center border-b bg-yellow-100 border-yellow-200'
+        className='flex flex-wrap p-3 items-center border-b bg-acLight border-acYellow'
       >
         <a className='navbar-brand mb-0 h1 text-xl text-center' href='/'>
           <span className='font-title font-extrabold'>The </span>Better Turnip
