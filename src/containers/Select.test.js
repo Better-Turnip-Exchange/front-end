d0d4e4007b648e16bb7ce9810ddd081eb1a8c6af
Daniel Williams <danswilliams95@gmail.com>
2020-05-21 00:00:51 -0700

    Repo cleanup.

--- src/containers/Select.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Select from './Select.js';
-import renderer from 'react-test-renderer';
-
-test('Select renders', () => {
-    const tree = renderer.create(<Select />).toJSON();
-    expect(tree).toMatchSnapshot();
-})
-
