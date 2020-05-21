0841de70b8173750a79ff8a33e72d3814bbaefac
Daniel Williams <danswilliams95@gmail.com>
2020-05-20 23:53:58 -0700

    Adding test for Splash page.

--- src/containers/Splash.js
+++ src/containers/Splash.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Splash = props => {
   return (
     <div className='flex justify-center items-center mt-10 md:mt-40'>
-      <div id='welcome-wrapper' className='card text-center p-10 mx-10'>
+      <div id='welcome-card' className='card text-center p-10 mx-10'>
         <h1 className='title'>The</h1>
         <h1 className='title'>Better Turnip Exchange</h1>
-        <a id='get-started-btn' className='btn btn-blue mt-4' href='/find'>
+        <a id='welcome-btn' className='btn btn-blue mt-4' href='/find'>
           Get Started
         </a>
       </div>
