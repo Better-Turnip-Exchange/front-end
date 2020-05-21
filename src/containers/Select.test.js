cc06f7addf712380a3a639108a4aa1e71bae40a6
Daniel Williams <danswilliams95@gmail.com>
2020-05-20 17:14:49 -0700

    Setting up snapshot testing.

---
+++ src/containers/Select.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Select from './Select.js';
+import renderer from 'react-test-renderer';
+
+test('Select renders', () => {
+    const tree = renderer.create(<Select />).toJSON();
+    expect(tree).toMatchSnapshot();
+})
